7cc9b9eb47e9af04770ab11ea4b52e1097f5bdbe
arachnothrone <js1000avf@gmail.com>
2019-05-15 21:12:07 -0400

    Modify routes

--- src/main/scala/webs/WebServer.scala
+++ src/main/scala/webs/WebServer.scala
@@ -10,10 +10,15 @@ object WebServer extends HttpApp {
             get {
                 complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http v2</h1>"))
             }
-        }~
-        path("asdf") {
+        } ~
+        path("two") {
             get {
-                complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>asdf</h1>"))
+                complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>two 2</h1>"))
             }
-        }
+        } ~
+        path("three") {
+            get {
+                complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>three 3</h1>"))
+            }
+    }
 }
